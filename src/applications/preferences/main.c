8c5876834e9bf931b7e592187d88c677f434a376
SipoczAdam <adamsipocz2006@gmail.com>
2026-05-21 18:59:08 +0200

    Date & Time settings layout

--- src/applications/preferences/main.c
+++ src/applications/preferences/main.c
@@ -95,8 +95,8 @@ void render_to_buffer(kernel_api_t* api, struct multiboot_tag_framebuffer* fb, s
     if (api->maximize_icon) api->draw_icon_scaled(max_x, max_y, max_size, max_size, api->maximize_icon, target_fb);
 
     // 5. Sidebar Menu Items
-    const char* menu_items[] = {"Wallpaper", "Network" ,"About"};
-    for (int i = 0; i < 3; i++) {
+    const char* menu_items[] = {"Wallpaper", "Network", "Date & Time", "About"};
+    for (int i = 0; i < 4; i++) {
         uint32_t item_y = title_bar_h + 20 + (i * 40);
         if (i == selected_menu) {
             api->draw_rect(10, item_y - 10, sidebar_w - 20, 35, 0xE0E0E0, target_fb);
@@ -199,7 +199,11 @@ void render_to_buffer(kernel_api_t* api, struct multiboot_tag_framebuffer* fb, s
         api->draw_string_scaled(value_x, ly, buf, 0x333333, 70, target_fb);
     }  
     
-    else if (selected_menu == 2) { // About
+    else if (selected_menu == 2) { // Date & Time
+        api->draw_string_scaled(cx, cy, "Date & Time", 0x222222, 90, target_fb);
+    }  
+
+    else if (selected_menu == 3) { // About
         api->draw_string_scaled(cx, cy, "About AnimOS", 0x222222, 90, target_fb);
         
         // Boot Logo - Centered between the actual end of text and the right window edge
